05a02458a1b48a9007b2c2eb659e4ad07a72728f
larsfinlay <larsfinlay@googlemail.com>
2019-07-18 10:18:46 +0100

    UpdateShould.ReturnUpdatedPerson :: Red

--- NET-Core-ASP-API.Tests/Services/PeopleServiceTests.cs
+++ NET-Core-ASP-API.Tests/Services/PeopleServiceTests.cs
@@ -62,13 +62,28 @@ namespace NETCoreASPAPI.Tests.Services
         [TestFixture]
         public class UpdateShould : PeopleServiceTests
         {
-            private Person _updateData = new Person { FirstName = "Bob", Surname = "Streets", Age = 23 };
+            private static string _firstName = "Bob";
+            private static string _updateSurname = "Streets";
+            private static int _age = 23;
+
+            private Person _updateData = new Person { FirstName = _firstName, Surname = _updateSurname, Age = _age };
+            private Person _updatedPerson = new Person { ID = 1, FirstName = _firstName, Surname = _updateSurname, Age = _age };
 
             [Test]
             public void CallUpdatePerson()
             {
                 service.UpdatePerson(1, _updateData);
                 peopleRepoMock.Verify(q => q.UpdatePerson(_updateData), Times.Once());
+            }
+
+            [Test]
+            public void ReturnUpdatedPerson()
+            {
+                peopleRepoMock.Setup(q => q.UpdatePerson(_updateData)).Returns(_updatedPerson);
+
+                var result = service.UpdatePerson(1, _updateData);
+
+                result.ShouldBe(_updatedPerson);
             }
         }
     }
